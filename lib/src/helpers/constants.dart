1a22d952e65ba378903011689474f69797a9d98f
xukang1983 <xukang1983@qq.com>
2021-04-18 23:32:39 +0800

    Initial commit

---
+++ lib/src/helpers/constants.dart
@@ -0,0 +1,5 @@
+class Constant{
+  static const String newsApiKey = 'API_KEY_HERE';
+  static const String baseUrl = 'https://newsapi.org/v2/';
+  static const String topHeadLine = '/top-headlines';
+}
